939262b365f11a0a3b21e8de1633878b011c5cfb
a.berriosh <a.berriosh@alumnos.duoc.cl>
2021-11-29 01:07:49 -0300

    modificación de addTask

--- WSProcesSa/Controllers/TareaController.cs
+++ WSProcesSa/Controllers/TareaController.cs
@@ -235,7 +235,7 @@ namespace WSProcesSa.Controllers
                                 PorcentajeAvance = 1,
                                 FechaCreacion = DateTime.Now,
                                 CreadaPor = newTareaToAdd.CreadaPor,
-                                FkRutUsuario = "0.000.000",
+                                FkRutUsuario = newTareaToAdd.FkRutUsuario,
                                 FkIdJustificacion = newTareaToAdd.FkIdJustificacion,
                                 FkEstadoTarea = 1,
                                 FkPrioridadTarea = newTareaToAdd.FkPrioridadTarea,
@@ -1127,5 +1127,11 @@ namespace WSProcesSa.Controllers
                 return StatusCode(500, response);
             }
         }
+
+
+        
+
+
+
     }
 }
